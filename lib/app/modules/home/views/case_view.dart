4036675c5dfb061d2fb5f0eedad78eb29a336da5
d1y <chenhonzhou@gmail.com>
2022-04-01 12:38:19 +0800

    feat: 切换线路提示信息

--- lib/app/modules/home/views/case_view.dart
+++ lib/app/modules/home/views/case_view.dart
@@ -1,3 +1,4 @@
+import 'package:dan211/config/dart_const.dart';
 import 'package:dan211/share/dan_movie_card.dart';
 import 'package:dan211/share/dan_movie_data.dart';
 import 'package:dan211/widget/k_list_tile.dart';
@@ -19,7 +20,21 @@ class DoMovieCaseView extends StatelessWidget {
         trailing: CupertinoButton(
           padding: EdgeInsets.zero,
           onPressed: () {
-            /// TODO impl this
+            showCupertinoDialog<void>(
+              context: context,
+              builder: (BuildContext context) => CupertinoAlertDialog(
+                title: const Text('声明'),
+                content: const Text(CASE_TIPS),
+                actions: <CupertinoDialogAction>[
+                  CupertinoDialogAction(
+                    child: const Text('爷知道了'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            );
           },
           child: const Icon(
             CupertinoIcons.zzz,
